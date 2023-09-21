226e54187e63e10343aff66484f9c2f87a88d7c0
celinech <chevalierceline2@gmail.com>
2023-09-21 10:36:13 +0200

    ADD .gitignore file

---
+++ checkDirHierarchy.R
@@ -0,0 +1,22 @@
+checkDirHierarchy <- function() {
+    # Check if mandatory directory already exist. If not, create them
+    if (!dir.exists(PATH_RDS_OBJECTS)) { 
+        print(paste("Create the rds objects directory: ", PATH_RDS_OBJECTS))
+        dir.create(PATH_RDS_OBJECTS)
+    }
+    
+    if (!dir.exists(PATH_ROOT)) { 
+        print(paste("Create the root directory: ", PATH_ROOT))
+        dir.create(PATH_ROOT)
+    }
+    
+    if (!dir.exists(PATH_OUT_HTML)) { 
+        print(paste("Create the html output files directory: ", PATH_OUT_HTML))
+        dir.create(PATH_OUT_HTML)
+    }
+    
+    if (!dir.exists(PATH_OUT_FIG)) { 
+        print(paste("Create the figures directory: ", PATH_OUT_FIG))
+        dir.create(PATH_OUT_FIG)
+    }
+}
