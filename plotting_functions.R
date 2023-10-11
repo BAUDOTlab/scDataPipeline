1100de737bff49689bf9024075053793de159cbd
Benjamin L <benjamin.LOIRE@univ-amu.fr>
2023-10-11 14:24:32 +0200

    added 4 helper functions

--- plotting_functions.R
+++ plotting_functions.R
@@ -94,6 +94,35 @@ highlightClusterPlot <- function(clusterName, seuratObject, reduction = "umap") 
 
 }
 
+######## Helper functions for dynamic display, better used in apply functions
+set_thresh <- function(thresh, i, metadata, metric){
+  cplt@meta.data[metadata > thresh, paste0(metric,"_groups")] <- paste0("gp",i,"_", metric)
+}
+
+threshline <- function(thresh){
+  geom_hline(aes(yintercept = thresh), color = "#FF0000AA", size = 1.5)
+}
+
+threshtext <- function(thresh, i, metric){
+  geom_text(aes(x=0.5, y=0.8*thresh, label=paste0("gp",i,"_",metric)))
+}
+
+threshlabel <- function(thresh){
+  res = c()
+  for(i in 1:length(thresh)){
+    if(i==1){
+      res <- append(res,paste0("x < ", thresh[i]))
+    } 
+    
+    if (i==length(thresh)) {
+      res <- append(res, paste0(thresh[i], " < x"))
+    } else {
+      res <- append(res, paste0(thresh[i], " < x < ", thresh[i+1]))
+    }
+  }
+  return(res)
+}
+
 
 ######## Function to create a dot plot for 40 markers, then will create a new dotPlot
 create_dot_plot <- function(SO, geneList, title=NULL) {
