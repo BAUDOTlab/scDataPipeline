f91f63085c87f96173f27858cf6da950344486ff
Benjamin L <benjamin.LOIRE@univ-amu.fr>
2023-11-15 14:01:54 +0100

    addin MELD rmd and convert script

--- convert.R
+++ convert.R
@@ -1,28 +1,27 @@
-library(Seurat)
-library(SeuratObject)
-library(SeuratDisk)
+convert <- function(inputFile){
+    library(Seurat)
+    library(SeuratObject)
+    library(SeuratDisk)
 
-myargs <- commandArgs(trailingOnly=TRUE)
+    outputFile <- gsub(".rds",".h5Seurat", inputfile)
 
-inputFile <- myargs[1] # ends by .rds
-outputFile <- myargs[2] # ends with .h5Seurat
+    cat("\n-----------------------------------------------------------------------------\n")
+    print(paste0("Conversion of the file ", inputFile))
+    cat("\n-----------------------------------------------------------------------------\n")
 
-cat("\n-----------------------------------------------------------------------------\n")
-print(paste0("Conversion of the file ", inputFile))
-cat("\n-----------------------------------------------------------------------------\n")
+    #' Load the Seurat Object
+    cat("Load Seurat Object (SO)\n\n")
+    SO <- readRDS(inputFile) 
 
-#' Load the Seurat Object
-cat("Load Seurat Object (SO)\n\n")
-SO <- readRDS(inputFile) 
+    SO$phase <- Idents(SO)
 
-SO$phase <- Idents(SO)
+    #' Save as a h5Seurat file format
+    cat("Save SO as a h5Seurat file\n\n")
+    SeuratDisk::SaveH5Seurat(SO, outputFile, overwrite = TRUE)
 
-#' Save as a h5Seurat file format
-cat("Save SO as a h5Seurat file\n\n")
-SeuratDisk::SaveH5Seurat(SO, outputFile, overwrite = TRUE)
+    #' Convert to a h5ad file format, readable by scanpy
+    cat("Do the conversion from h5Seurat to h5ad file format\n\n")
+    SeuratDisk::Convert(outputFile, dest = "h5ad", assay = "integrated", overwrite = TRUE)
 
-#' Convert to a h5ad file format, readable by scanpy
-cat("Do the conversion from h5Seurat to h5ad file format\n\n")
-SeuratDisk::Convert(outputFile, dest = "h5ad", assay = "integrated", overwrite = TRUE)
-
-file.remove(outputFile)
+    file.remove(outputFile)
+}
