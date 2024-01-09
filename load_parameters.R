258933f783225eac23e79d812e35977ad32356cb
celinech <chevalierceline2@gmail.com>
2024-01-09 10:04:52 +0100

    Dev: New feature to display manual annotation on Meld plots

--- load_parameters.R
+++ load_parameters.R
@@ -18,6 +18,8 @@ load_parameters <- function(config_file) {
     assign("PATH_OUT_HTML",     	file.path(PATH_ROOT, path$output$PATH_OUT_HTML), 			envir = .GlobalEnv)
     assign("PATH_OUT_FIG",      	file.path(PATH_ROOT, path$output$PATH_OUT_FIG, DATASET),	envir = .GlobalEnv)
     assign("PATH_GENES_OF_INTEREST",file.path(PATH_ROOT, path$input$PATH_GENES_OF_INTEREST),	envir = .GlobalEnv)
+    assign("PATH_MANUAL_ANNOTATION",file.path(PATH_ROOT, path$input$PATH_MANUAL_ANNOTATION),	envir = .GlobalEnv)
+    
 
     # parse the qc variables
     if (exists("qc") && is.list(qc)) {
