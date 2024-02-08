98b8c199853e2a58e872e348a13fe658a88953dc
sevdarafatov <sevda.rafatova@gmail.com>
2024-02-08 16:26:00 +0000

    Adding sanity check for loading parameters

--- load_parameters.R
+++ load_parameters.R
@@ -16,17 +16,58 @@ load_parameters <- function(config_file) {
         assign("PATH_ROOT", path$PATH_ROOT, envir = .GlobalEnv)        
     }
 
-    assign("PATH_INPUT_LABDATA",	file.path(PATH_ROOT, path$input$PATH_INPUT_LABDATA),		envir = .GlobalEnv)
-	assign("PATH_ATLAS",         	file.path(PATH_ROOT, path$input$PATH_ATLAS), 				envir = .GlobalEnv)
-    assign("PATH_ATLAS_FILE",       file.path(PATH_ATLAS, path$input$PATH_ATLAS_FILE), 		    envir = .GlobalEnv)
-    assign("PATH_RDS_OBJECTS",  	file.path(PATH_ROOT, path$output$PATH_RDS_OBJECTS), 		envir = .GlobalEnv)
-    assign("PATH_OUT_HTML",     	file.path(PATH_ROOT, path$output$PATH_OUT_HTML), 			envir = .GlobalEnv)
-    assign("PATH_OUT_FIG",      	file.path(PATH_ROOT, path$output$PATH_OUT_FIG, DATASET),	envir = .GlobalEnv)
-    assign("PATH_GENES_OF_INTEREST",file.path(PATH_ROOT, path$input$PATH_GENES_OF_INTEREST),	envir = .GlobalEnv)
+    if ("PATH_INPUT_LABDATA" %in% names(path$input)) {
+        assign("PATH_INPUT_LABDATA",file.path(PATH_ROOT, path$input$PATH_INPUT_LABDATA),		envir = .GlobalEnv)
+        cat("Realpath of PATH_INPUT_LABDATA:", normalizePath(PATH_INPUT_LABDATA), "\n")
+    } else {
+        cat("The PATH_INPUT_LABDATA is not provided\n")
+    }
+
+    if ("PATH_ATLAS" %in% names(path$input)) {
+        assign("PATH_ATLAS",file.path(PATH_ROOT, path$input$PATH_ATLAS), 				envir = .GlobalEnv)
+        cat("Realpath of PATH_ATLAS:", normalizePath(PATH_ATLAS), "\n")
+    } else {
+        cat("The PATH_ATLAS is not provided\n")
+    }
+
+    if ("PATH_ATLAS_FILE" %in% names(path$input)) {
+        assign("PATH_ATLAS_FILE",file.path(PATH_ATLAS, path$input$PATH_ATLAS_FILE), 		    envir = .GlobalEnv)
+        cat("Realpath of PATH_ATLAS_FILE:", normalizePath(PATH_ATLAS_FILE), "\n")
+    } else {
+        cat("The PATH_ATLAS_FILE is not provided\n")
+    }
+
+    if ("PATH_RDS_OBJECTS" %in% names(path$output)) {
+        assign("PATH_RDS_OBJECTS",file.path(PATH_ROOT, path$output$PATH_RDS_OBJECTS), 		envir = .GlobalEnv)
+        cat("Realpath of PATH_RDS_OBJECTS:", normalizePath(PATH_RDS_OBJECTS), "\n")
+    } else {
+        cat("The PATH_RDS_OBJECTS is not provided\n")
+    }
+
+    if ("PATH_OUT_HTML" %in% names(path$output)) {
+        assign("PATH_OUT_HTML",file.path(PATH_ROOT, path$output$PATH_OUT_HTML), 			envir = .GlobalEnv)
+        cat("Realpath of PATH_OUT_HTML:", normalizePath(PATH_OUT_HTML), "\n")
+     } else {
+        cat("The PATH_OUT_HTML is not provided\n")
+    }
+
+    if ("PATH_OUT_FIG" %in% names(path$output)) {
+        assign("PATH_OUT_FIG",file.path(PATH_ROOT, path$output$PATH_OUT_FIG, DATASET),	envir = .GlobalEnv)
+        cat("Realpath of PATH_OUT_FIG:", normalizePath(PATH_OUT_FIG), "\n")
+     } else {
+        cat("The PATH_OUT_FIG is not provided\n")
+    }
+
+    if ("PATH_GENES_OF_INTEREST" %in% names(path$input)) {
+        assign("PATH_GENES_OF_INTEREST",file.path(PATH_ROOT, path$input$PATH_GENES_OF_INTEREST),	envir = .GlobalEnv)
+        cat("Realpath of PATH_GENES_OF_INTEREST:", normalizePath(PATH_GENES_OF_INTEREST), "\n")
+     } else {
+        cat("The PATH_GENES_OF_INTEREST is not provided\n")
+    }
+    
     if (exists("PATH_MANUAL_ANNOTATION")) {
         assign("PATH_MANUAL_ANNOTATION",file.path(PATH_ROOT, path$input$PATH_MANUAL_ANNOTATION),	envir = .GlobalEnv)
     }
-    
 
     # parse the qc variables
     if (exists("qc") && is.list(qc)) {
