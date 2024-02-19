2f5501967d67d2889ae6e680f6dd9cb7d6289408
celinech <chevalierceline2@gmail.com>
2024-02-19 13:23:19 +0000

    feat: factorization of the code

--- load_parameters.R
+++ load_parameters.R
@@ -10,63 +10,36 @@ load_parameters <- function(config_file) {
     # parse the paths variables
 
     # If the environment variable SCDP_PATH_ROOT exists, we are in a docker env and use it as the root
-    if(Sys.getenv("SCDP_PATH_ROOT") != ""){
+    if (Sys.getenv("SCDP_PATH_ROOT") != "") {
         assign("PATH_ROOT", Sys.getenv("SCDP_PATH_ROOT"), envir = .GlobalEnv)
     } else {
-        assign("PATH_ROOT", path$PATH_ROOT, envir = .GlobalEnv)        
+        assign("PATH_ROOT", path$PATH_ROOT, envir = .GlobalEnv)
     }
 
-    if ("PATH_INPUT_LABDATA" %in% names(path$input)) {
-        assign("PATH_INPUT_LABDATA",file.path(PATH_ROOT, path$input$PATH_INPUT_LABDATA),		envir = .GlobalEnv)
-        cat("Realpath of PATH_INPUT_LABDATA:", normalizePath(PATH_INPUT_LABDATA), "\n")
-    } else {
-        cat("The PATH_INPUT_LABDATA is not provided\n")
+    # Function to check for the presence of input and output paths, and then
+    # assign their full paths to global environment variables
+    checkAndAssignPath <- function(pathName, pathValue) {
+        if (pathName %in% names(path$input) || pathName %in% names(path$output)) {
+            fullPath <- file.path(PATH_ROOT, pathValue)
+            assign(pathName, fullPath, envir = .GlobalEnv)
+            cat(paste("Realpath of", pathName, ":", normalizePath(fullPath), "\n"))
+            return(TRUE)
+        } else {
+            cat(paste("The", pathName, "is not provided\n"))
+            return(FALSE)
+        }
     }
 
-    if ("PATH_ATLAS" %in% names(path$input)) {
-        assign("PATH_ATLAS",file.path(PATH_ROOT, path$input$PATH_ATLAS), 				envir = .GlobalEnv)
-        cat("Realpath of PATH_ATLAS:", normalizePath(PATH_ATLAS), "\n")
-    } else {
-        cat("The PATH_ATLAS is not provided\n")
-    }
-
-    if ("PATH_ATLAS_FILE" %in% names(path$input)) {
-        assign("PATH_ATLAS_FILE",file.path(PATH_ATLAS, path$input$PATH_ATLAS_FILE), 		    envir = .GlobalEnv)
-        cat("Realpath of PATH_ATLAS_FILE:", normalizePath(PATH_ATLAS_FILE), "\n")
-    } else {
-        cat("The PATH_ATLAS_FILE is not provided\n")
-    }
-
-    if ("PATH_RDS_OBJECTS" %in% names(path$output)) {
-        assign("PATH_RDS_OBJECTS",file.path(PATH_ROOT, path$output$PATH_RDS_OBJECTS), 		envir = .GlobalEnv)
-        cat("Realpath of PATH_RDS_OBJECTS:", normalizePath(PATH_RDS_OBJECTS), "\n")
-    } else {
-        cat("The PATH_RDS_OBJECTS is not provided\n")
-    }
-
-    if ("PATH_OUT_HTML" %in% names(path$output)) {
-        assign("PATH_OUT_HTML",file.path(PATH_ROOT, path$output$PATH_OUT_HTML), 			envir = .GlobalEnv)
-        cat("Realpath of PATH_OUT_HTML:", normalizePath(PATH_OUT_HTML), "\n")
-     } else {
-        cat("The PATH_OUT_HTML is not provided\n")
-    }
-
-    if ("PATH_OUT_FIG" %in% names(path$output)) {
-        assign("PATH_OUT_FIG",file.path(PATH_ROOT, path$output$PATH_OUT_FIG, DATASET),	envir = .GlobalEnv)
-        cat("Realpath of PATH_OUT_FIG:", normalizePath(PATH_OUT_FIG), "\n")
-     } else {
-        cat("The PATH_OUT_FIG is not provided\n")
-    }
-
-    if ("PATH_GENES_OF_INTEREST" %in% names(path$input)) {
-        assign("PATH_GENES_OF_INTEREST",file.path(PATH_ROOT, path$input$PATH_GENES_OF_INTEREST),	envir = .GlobalEnv)
-        cat("Realpath of PATH_GENES_OF_INTEREST:", normalizePath(PATH_GENES_OF_INTEREST), "\n")
-     } else {
-        cat("The PATH_GENES_OF_INTEREST is not provided\n")
-    }
-    
+    # Check and assign specific paths
+    checkAndAssignPath("PATH_INPUT_LABDATA", path$input$PATH_INPUT_LABDATA)
+    checkAndAssignPath("PATH_ATLAS", path$input$PATH_ATLAS)
+    checkAndAssignPath("PATH_ATLAS_FILE", path$input$PATH_ATLAS_FILE)
+    checkAndAssignPath("PATH_RDS_OBJECTS", path$output$PATH_RDS_OBJECTS)
+    checkAndAssignPath("PATH_OUT_HTML", path$output$PATH_OUT_HTML)
+    checkAndAssignPath("PATH_OUT_FIG", file.path(path$output$PATH_OUT_FIG, DATASET))
+    checkAndAssignPath("PATH_GENES_OF_INTEREST", path$input$PATH_GENES_OF_INTEREST)
     if (exists("PATH_MANUAL_ANNOTATION")) {
-        assign("PATH_MANUAL_ANNOTATION",file.path(PATH_ROOT, path$input$PATH_MANUAL_ANNOTATION),	envir = .GlobalEnv)
+        checkAndAssignPath("PATH_MANUAL_ANNOTATION", path$input$PATH_MANUAL_ANNOTATION)
     }
 
     # parse the qc variables
